6d54355b2ef675ebe50e40c73ee6bfecdf2ffe19
furkan.portakal <furkan.portakal@parga.li>
2022-06-07 22:50:01 +0300

    fix: logo redirect

--- infrastructure/layout/header/components/logo.js
+++ infrastructure/layout/header/components/logo.js
@@ -1,9 +1,12 @@
 import { Logo } from '@svg'
+import Link from 'next/link'
 
 export default function LogoWrapper() {
     return (
         <div className='application-header-logo'>
-            <Logo />
+            <Link href="/">
+                <Logo />
+            </Link>
         </div>
     )
 }
