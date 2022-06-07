22d606cc14f27a8bfaf4519b561d39c3cefa267e
furkan.portakal <furkan.portakal@parga.li>
2022-06-07 22:45:03 +0300

    profile progress

--- infrastructure/components/profile/components/blogs.js
+++ infrastructure/components/profile/components/blogs.js
@@ -1,5 +1,11 @@
+import styles from './blogs.module.css'
+
 export default function blogs() {
     return (
-        <div>blogs</div>
+        <div className={styles.container}>
+            <h1 className={styles.title}>Bloglar</h1>
+            <img className={styles.langingBlogs} src='/blogs.png' />
+            <img className={styles.langingBlogs} src='/blogs.png' />
+        </div>
     )
 }
